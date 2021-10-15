36bd063a75fe3c9ee761ac5d29843e9fcec415cd
daniel <daniel adolfsson>
2021-10-15 16:20:54 +0200

    added docker fiels

--- docker_build.sh
+++ docker_build.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 #
-# This script runs docker build to create the pytorch-gpu.
+# Usage:  ./docker_run.sh [/path/to/data]
 #
+# This script calls `nvidia-docker run` to start the mask-rcnn
+# container with an interactive bash session.  This script sets
+# the required environment variables and mounts the labelfusion
+# source directory as a volume in the docker container.  If the
+# path to a data directory is given then the data directory is
+# also mounted as a volume.
+# 
+# -v mount folder into container
 
-set -exu
-nvidia-docker build --tag ynyg/torch-gpu ./
+
+
+nvidia-docker run --name pytorch_jupyter -it --rm  -v /home/dlan/:/tf -e NB_UID=1000 -e NB_GID=1000 -e VNC_SERVER_PASSWORD=password -p 8888:8888  --runtime=nvidia -p 6006:6006 -p 5900:5900 tensorflow/tensorflow:latest-gpu-jupyter 
+
--- docker_run.sh
+++ docker_run.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 #
-# Usage:  ./docker_run.sh [/path/to/data]
+# This script runs docker build to create the pytorch-gpu.
 #
-# This script calls `nvidia-docker run` to start the mask-rcnn
-# container with an interactive bash session.  This script sets
-# the required environment variables and mounts the labelfusion
-# source directory as a volume in the docker container.  If the
-# path to a data directory is given then the data directory is
-# also mounted as a volume.
-# 
-# -v mount folder into container
 
-image_name=ynyg/torch-gpu:latest
-
-NOTEBOOKS_PATH=/media/local-data/yuxuan/notebooks
-nvidia-docker run --name pytorch_jupyter -it --rm -v $NOTEBOOKS_PATH:/notebooks -v /home/ynyg/yuxuan/:/repository -v /home/ynyg/Data/:/repository/Data -e NB_UID=1000 -e NB_GID=1000 -e VNC_SERVER_PASSWORD=password -p 8888:8888 --runtime=nvidia -p 6006:6006 -p 5900:5900 ynyg/torch-gpu:latest
-
+docker pull tensorflow/tensorflow:latest-gpu-jupyter
